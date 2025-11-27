60d4052f7cb852f355577c6f9ea35b39584f2385
Brice Friha <contact@bricefriha.dev>
2025-11-27 20:21:39 +0000

    barebone shit

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Yo, Acura")
+	var remote string
+	framework := "flutter"
+
+	for i := 0; i < len(os.Args); i++ {
+		arg := os.Args[i]
+		if len(arg) > 0 {
+			switch arg {
+			case "-r":
+				if i+1 < len(os.Args) {
+					remote = string(os.Args[i+1])
+				}
+			case "-f":
+				if i+1 < len(os.Args) {
+					framework = string(os.Args[i+1])
+				}
+
+			}
+
+		}
+	}
+	fmt.Println("Remote:", remote)
+	fmt.Println("Framework:", framework)
 }
