5f101e631afbfdc285d7ddb07760c5198412f23c
Brice Friha <contact@bricefriha.dev>
2025-11-27 20:27:07 +0000

    introduce a argsCount variable

--- main.go
+++ main.go
@@ -8,17 +8,19 @@ import (
 func main() {
 	var remote string
 	framework := "flutter"
+	argsCount := len(os.Args)
 
-	for i := 0; i < len(os.Args); i++ {
+	for i := 0; i < argsCount; i++ {
 		arg := os.Args[i]
+
 		if len(arg) > 0 {
 			switch arg {
 			case "-r":
-				if i+1 < len(os.Args) {
+				if i+1 < argsCount {
 					remote = string(os.Args[i+1])
 				}
 			case "-f":
-				if i+1 < len(os.Args) {
+				if i+1 < argsCount {
 					framework = string(os.Args[i+1])
 				}
 
