68ff168c352306d1ad63e8509801e5ff436f38fb
Brice Friha <contact@bricefriha.dev>
2025-11-28 23:55:18 +0000

    managing rsync command

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
 )
 
 func main() {
 	var remote string
 	framework := "flutter"
 	argsCount := len(os.Args)
+	projName := "my_flutter_project" // fake for testing
 
 	for i := 0; i < argsCount; i++ {
 		arg := os.Args[i]
@@ -23,11 +25,27 @@ func main() {
 				if i+1 < argsCount {
 					framework = string(os.Args[i+1])
 				}
-
 			}
 
 		}
 	}
 	fmt.Println("Remote:", remote)
 	fmt.Println("Framework:", framework)
+	cmd := exec.Command("rsync", "-avz",
+		"--include="+projName+"/pubspec.yaml",
+		"--include="+projName+"/pubspec.lock",
+		"--include="+projName+"/lib/***",
+		"--include="+projName+"/assets/***",
+		"--include="+projName+"/ios/***",
+		"--include=lib/config/*",
+		"--exclude="+projName+"/*",
+		"../",
+		fmt.Sprintf("%s:~/Documents/%s", remote, projName),
+	)
+	output, err := cmd.Output()
+	if err != nil {
+		fmt.Println("Error executing rsync:", err)
+		return
+	}
+	fmt.Println("Result:", output)
 }
